08f827cad803a42edec80dd38ca1dc5a27a78c67
MaxLettner <lettnermaximilian2008@gmail.com>
2026-04-17 17:13:15 +0200

    feat: implemented coordinate transmission from backend to frontend

--- Frontend/src/services/coordinate.service.ts
+++ Frontend/src/services/coordinate.service.ts
@@ -10,14 +10,14 @@ let latestCoord : coord
 const socket = new WebSocket('ws://127.0.0.1:8000/api/stream/coordinates')
 
 socket.addEventListener('message', (event) => {
-  latestCoord = event.data
-  console.log(latestCoord)
+  latestCoord = JSON.parse(event.data)
+  //console.log(latestCoord)
 })
 
 export const getLatestPosition = (): coord => {
-  //return latestCoord;
+  return latestCoord;
     
-  return { x: 100, y: 100, z: Math.random() * 200, timestamp:'2026-02-20T17:56:56.939303+00:00'}
+  //return { x: 100, y: 100, z: Math.random() * 200, timestamp:'2026-02-20T17:56:56.939303+00:00'}
 }
 
 export const getScale = (): number => {
