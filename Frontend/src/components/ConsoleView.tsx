19fbb669aa2b574bec2b11411c8d9d2292a609ae
MaxLettner <lettnermaximilian2008@gmail.com>
2026-05-29 18:10:35 +0200

    feat: changed ConsoleView to accept multiple coords at once

--- Frontend/src/components/ConsoleView.tsx
+++ Frontend/src/components/ConsoleView.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import type ICoord from "../services/coordinate.service"
 import '../styles/ConsoleView.css'
 
@@ -6,18 +6,26 @@ interface Props {
   data: ICoord[][]
 }
 
-const ConsoleView = ({ data }: Props) => {
+const ConsoleView = ({data}: Props) => {
+  const consoleRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (consoleRef.current) {
+      consoleRef.current.scrollTop = consoleRef.current.scrollHeight
+    }
+  }, [data])
+
   return (
-    <div className='consoleView'>
+    <div className='consoleView' ref={consoleRef}>
       {data.map((e, outerIndex) =>
         e.map((c, innerIndex) => (
           <React.Fragment key={`${outerIndex}-${innerIndex}`}>
-            {innerIndex === 0 ? '>' : '\u00A0'} X: {c.x.toFixed(2)} Y: {c.y.toFixed(2)} Z: {c.z.toFixed(2)} Time: {c.timestamp.slice(11,19)}<br />
+            {innerIndex === 0 ? '>' : '\u00A0'} X: {c.x.toFixed(2)} Y: {c.y.toFixed(2)} Z: {c.z.toFixed(2)} Time: {c.timestamp.slice(11,19)}<br/>
           </React.Fragment>
         ))
       )}
     </div>
-  );
-};
+  )
+}
 
 export default ConsoleView
